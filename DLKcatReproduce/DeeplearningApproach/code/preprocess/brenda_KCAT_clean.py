c8ab03de00cb8e526bcf67b94b6eb1d08d427261
rainstar100 <23122842@qq.com>
2024-10-06 08:39:56 +0800

    none

---
+++ DLKcatReproduce/DeeplearningApproach/code/preprocess/brenda_KCAT_clean.py
@@ -0,0 +1,55 @@
+'''
+clean  the raw data from brenda website  which in KCAT file
+'''
+import os
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+from tools.parallel import parallel
+from tools.decorator import timetry
+
+#clean data
+@timetry
+def cleanFile(file_name):
+    '''
+    read-clean-save
+    '''
+
+    #read file
+    file=input_path+file_name #set filepath
+    with open(file,'r',encoding='utf-8') as f:
+        raw_data=f.read()
+        print(len(raw_data))
+    #clean file
+    soup = BeautifulSoup(raw_data, 'lxml')
+    tags=soup.find_all('div',id=re.compile("^(tab44_head|tab44r)"))
+    data_list=[]
+    for tag in tags:
+        row=tag.get_text().split('\n')
+        #print(row)
+        if len(row)==9 and row[4]!='-' and row[4]!='UNIPROT':
+            data_list.append(row)
+        else:
+            #print('delete')
+            pass
+    #save data
+    df=pd.DataFrame(data_list)
+    outfile=output_path+file_name[:-3]+'csv'
+    df.to_csv(outfile,encoding='utf-8',index=False)
+    print('saved')
+          
+    return data_list
+
+
+if __name__=='__main__':
+
+    # set path 
+    input_path='./Data/database/brenda_raw/KCAT/'
+    output_path='./Data/database/brenda_csv/'
+
+    #read the raw data 
+    ##Read all BRENDA file names:
+    file_names = os.listdir(input_path)[:1]
+    #cleanFile(file_names)
+    parallel(cleanFile,file_names)  #munipule threads to cleanHtml
+    
